13adba090530e12b486f9b5a00fad576dabaadea
Tengen <leha.denisov.03@mail.ru>
2024-04-07 09:56:58 +0300

    Update ConsoleApplication6.cpp

--- ConsoleApplication6.cpp
+++ ConsoleApplication6.cpp
@@ -1,24 +1,27 @@
-﻿#include <opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
-
 using namespace cv;
 using namespace std;
-
-int main() 
+void findTextContours(const Mat& inputImage, Mat& outputImage) 
 {
-    Mat image = imread("image2.jpg");
     Mat gray, blurred, edges;
-    cvtColor(image, gray, COLOR_BGR2GRAY);
+    cvtColor(inputImage, gray, COLOR_BGR2GRAY);
     GaussianBlur(gray, blurred, Size(5, 5), 0);
     Canny(blurred, edges, 30, 150);
     vector<vector<Point>> contours;
     vector<Vec4i> hierarchy;
-    findContours(edges.clone(), contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    Mat contour_image = Mat::zeros(image.size(), CV_8UC3);
-    for (size_t i = 0; i < contours.size(); i++) 
+    findContours(edges.clone(), contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+    for (int i = 0; i < contours.size(); i++) 
     {
-        drawContours(contour_image, contours, static_cast<int>(i), Scalar(0, 255, 0), 1);
+        Scalar color(0, 255, 0);
+        drawContours(outputImage, contours, i, color, 2, LINE_8, hierarchy, 0);
     }
+}
+int main()
+{
+    Mat image = imread("image2.jpg");
+    Mat contour_image = Mat::zeros(image.size(), CV_8UC3);
+    findTextContours(image, contour_image);
     imshow("Text Contours", contour_image);
     waitKey(0);
     return 0;
